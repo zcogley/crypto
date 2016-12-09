13a59f53496b3338b6bd03b7e66ee61a0c5d25c6
Zac Cogley <zaccogley@gmail.com>
2016-12-08 19:01:51 -0600

    slight mod

--- caesar.py
+++ caesar.py
@@ -3,10 +3,8 @@ from helpers import alphabet_position, rotate_character
 
 def user_input_is_valid(cl_args):
     if len(cl_args) != 2:
-        print("usage: python3 caesar.py n")
         return False
     if cl_args[1].isdigit() == False:
-        print("usage: python3 caesar.py n")
         return False
     return True
 
@@ -19,6 +17,7 @@ def encrypt(text, rot):
 
 def main():
     if user_input_is_valid(argv) == False:
+        print("usage: python3 caesar.py n")
         exit()
     print(encrypt(input("Type a message:\n"), int(argv[1])))
 
